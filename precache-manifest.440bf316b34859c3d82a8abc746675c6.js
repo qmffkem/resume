1e64dc420e051f6b420f7cd329c59f41cddff658
qmffkem <jaeung@vt.edu>
2019-12-27 23:05:38 -0500

    Updates

---
+++ precache-manifest.440bf316b34859c3d82a8abc746675c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa41f2ca3f3446257ef193038f14463f",
+    "url": "/please/index.html"
+  },
+  {
+    "revision": "8e9c08a537c23540a29e",
+    "url": "/please/static/css/main.3f3d6677.chunk.css"
+  },
+  {
+    "revision": "ce8ea570d16570799e3a",
+    "url": "/please/static/js/2.e2ed012e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/please/static/js/2.e2ed012e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e9c08a537c23540a29e",
+    "url": "/please/static/js/main.338a5a8e.chunk.js"
+  },
+  {
+    "revision": "28da7a16331c7d7fbfeb",
+    "url": "/please/static/js/runtime-main.2fd6dc0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/please/static/media/logo.5d5d9eef.svg"
+  }
+]);
