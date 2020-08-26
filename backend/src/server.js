e36dcbb052f937d712312677b70d032f6a7a0000
Jaeung Jang <jaeungj@Jaeungs-MBP.fios-router.home>
2020-08-26 17:37:39 -0400

    tries to implement stuff, but failed..

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,18 @@
 import express from "express";
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import {MongoClient} from 'mongodb';
 import path from 'path';
+import passport from 'passport';
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json());
 
-
 const withDB = async (operations, res) => {
     try{
+      // const client = mongoose.connect("mongodb://localhost:27017",{useUnifiedTopology: true});
       const client = await MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true});
       const db = client.db('resume');
 
@@ -22,13 +24,17 @@ const withDB = async (operations, res) => {
     }
 }
 
-app.get('/login', (req,res)=>{
-    withDB( async (db) => {
-        const articleName = req.params.name;
+// app.post('/login',passport.authenticate('local', { failureRedirect: '/login' }),(req,res)=>{
+//     withDB( async (db) => {
+//         const articleName = req.body.name;
     
-        const articleInfo = await db.collection('users').findOne({name : articleName});
-        res.status(200).json(articleInfo);
-      }, res);
+//         const articleInfo = await db.collection('users').findOne({name : articleName});
+//         res.status(200).json(articleInfo);
+//       }, res);
+// });
+
+app.post('/login', passport.authenticate('local'),(req,res)=>{
+  res.redirect('/users/' + req.user.username);
 });
 
 app.get('*', (req,res) =>{
