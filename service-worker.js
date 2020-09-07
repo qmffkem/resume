f78a384b596eb6159924868386ba65b68b81f047
Jaeung Jang <jaeungj@Jaeungs-MBP.fios-router.home>
2020-09-06 20:51:08 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.e26143d57f32627795e701b0d744e887.js"
+  "/resume/precache-manifest.f1cb4c7698ed435abda1c37126dd8570.js"
 );
 
 self.addEventListener('message', (event) => {
